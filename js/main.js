b24c5803e928dcaef2fb9a43118314e7e48d70be
rzatalibov555 <sethubpro@gmail.com>
2023-10-21 17:18:52 +0400

    js variables, operators, datatypes

---
+++ js/main.js
@@ -0,0 +1,92 @@
+
+// document.getElementById('parvin').innerText = "<span style='color:red'>Rza</span>"
+// document.getElementById('parvin2').innerHTML = "<span style='color:red'>Rza</span>"
+
+
+
+
+// var ad, soyad, yash
+
+
+// var ad;
+// console.log(ad)
+
+// ad = 5 
+// console.log(ad)
+
+
+
+// var a = 100
+
+// var b = a
+
+
+// ad = "Parvin"
+// soyad = "Qehremanli"
+// yash = '25'
+
+// document.write(ad + " " + soyad + " " + yash + " yasinda")
+
+
+
+
+// var a = 5
+// var b = 10
+// var c = 20
+
+// var d = (a + c) * b
+
+// console.log(d)
+
+// var a = "5"
+// var b = "10"
+
+// console.log(a + b)
+
+
+// var a
+// console.log(typeof a)
+
+
+// Data types
+
+// var a = 5                               //number  //num //integer //int
+// var b = "5"                             //string  //str //text
+// var $c = "Parvin"
+// var d = true                            //boolean //bool //true or false
+// var f = null                            //object  // obj // null
+
+// let y = 123e7;                          //bigint  //boyuk reqemler
+// let z = BigInt(1230500000)              //bigint  //boyuk reqemler
+
+// var demo = {}                           //object //obj
+// var nemo = []                           //object //obj //Massiv //Array //arr
+// var tarix = new Date()                  //object //obj //tarix
+
+// var test = Symbol("Demo")               //Symbol
+// console.log(tarix.getDate())
+// console.log(tarix.getHours())
+// console.log(tarix.getMinutes())
+
+// var telebeler = ['Rza', 'Pervin']
+// console.log(telebeler[0]+ " " + telebeler[1])
+
+// var demo = {                            
+//     name: "Parvin",
+//     surname: "Qehremanli",
+// }
+                  
+// document.write(test.toString())
+
+
+// kurs = "setHub"
+
+
+
+
+
+var my_name = "Parvin"
+var my_surname = "Qehremanli"
+
+console.log("Menim adim " + my_name + " " + my_surname )
+console.log(`Menim adim ${my_name} ${my_surname}dir. Menim 25 yasim var men ${my_surname} holdinqin direktoruyam`)
