dd4488aca9bd9190527b3253d11d0b00447492a2
rzatalibov555 <sethubpro@gmail.com>
2023-10-28 20:18:40 +0400

    operators

---
+++ js/operators.js
@@ -0,0 +1,104 @@
+
+// var a = 1
+// console.log(a + (a++))
+
+// let x = 5;
+// x++;
+// x++;
+// x++;
+// let z = x;
+// console.log(z)
+
+
+// for(x = 10; x >= 5; x--){
+//     console.log(x)
+// }
+
+
+// let x = 5
+
+// x = x + 10  // uzun
+// x += 10     // qisa
+
+// let y = 10
+// console.log(y/=20) 
+
+// console.log(x)
+// console.log(x += 10)
+
+
+
+
+
+// !   - deyilse ve ya yoxdursa
+
+// =   - menimsedir
+
+// ==  - beraber olub olmadigini yoxlayir (tipini yoxlamir)
+// !=  - beraber deyilse (tipini yoxlamir)
+
+// === - beraber olub olmadigini yoxlayir (tipini yoxlayir)
+// !== - beraber deyilse (tipini yoxlayir)
+
+// &&  - ve
+// ||  - veya
+
+
+// let x = 10
+// let y = 10
+
+// x >= y ? console.log("A") : console.log("B")   //// ternary operator
+
+// if(x >= y){
+//     console.log("A")   
+// }else{
+//     console.log("B")
+// }
+
+
+
+
+
+
+
+
+username = "Pervin"
+password = "555"
+Status = false
+
+db_username = "Pervin"
+db_password = "555"
+
+
+
+if(Status == true || username === db_username && password === db_password ){
+    console.log("succesfull")
+}else{
+    alert("Username ve ya password yalnisdir")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(username === db_username){
+    
+//     if(password === db_password){
+//         console.log("succesfull")
+//     }else{
+//         alert("password yanlisdir")
+//     }
+    
+// }else{
+//     alert("username yanlisdir")
+// }
+
+
