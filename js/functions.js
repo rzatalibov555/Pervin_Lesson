6550e9d4699fb738a3d56e3e95a3f6ca8e5f3614
rzatalibov555 <sethubpro@gmail.com>
2023-11-10 22:55:04 +0400

    parametrli funksiya

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,18 @@
-// function addData(){
-
-//     let a = document.getElementById("input1");
-//     let b = document.getElementById("myText");
-
-//     b.innerHTML = a.value;
-//     a.value = ""
-// }
-
-
-        
-
-// function addData( soyad, ad = "Rza", yaw = "18", Status = "Telebe"){
-//     console.log(`Menim adim ${ad} ${soyad}. Mənim ${yaw} yaşım var. Statusu: ${Status}`)
-//     // console.log("Menim adim "+ ad + " " + soyad + ". Mənim " + yaw + " yaşım var.")
-// }
-
-
-// addData("Talibov")
-
-
-// function chBg(){
-//     document.getElementById('demo1').style.backgroundColor = "blue";
-// }
-
-// function chBg2(){
-//     document.getElementById('demo2').style.backgroundColor = "purple";
-// }
-
-// function chBg3(){
-//     document.getElementById('demo3').style.backgroundColor = "green";
-// }
 
 
 
-function chBgPro(e){
-    e.style.background = "transparent";
+function blue(e, color){
+
+
+    e.style.backgroundColor = color;
 }
 
+
+
+function student(name, surname, age){
+    console.log(`Menim adim ${name} soyadim ${surname} ve yaşim ${age}`);
+}
+
+function goBack(e,defaultColor){
+    e.style.backgroundColor = defaultColor
+}
