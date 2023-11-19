ff606013b111a8efe9e0eca00fbc8a630d0b3915
rzatalibov555 <sethubpro@gmail.com>
2023-11-19 17:25:02 +0400

    selectors

---
+++ js/selector.js
@@ -0,0 +1,24 @@
+
+// document.getElementById('demo').innerHTML = "test"
+// document.getElementById('demo2').innerHTML = "123"
+
+
+// document.getElementsByClassName('a1')[0].innerHTML = "salam"
+// document.getElementsByClassName('a1')[2].innerHTML = "necesen?"
+// document.getElementsByClassName('a1')[3].innerHTML = "Yaxsiyam"
+// document.getElementsByClassName('a1')[1].innerHTML = "Ela"
+
+// document.getElementsByTagName('P')[0].innerHTML = "Super"
+// document.getElementsByTagName('P')[3].innerHTML = "Ela"
+
+// document.querySelector('#demo').innerHTML = "necesen"
+
+// let myTag = document.querySelectorAll('div > p')
+
+// console.log(myTag.length)
+
+// for(let x = 0; x < 2; x++){
+
+//     myTag[u].style.backgroundColor = 'red'
+
+// }
